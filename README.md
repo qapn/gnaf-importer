f2decfda3de52478b1631ca1a87f4aa26bea2340
Andrew Passarello <apassarello@gmail.com>
2018-12-05 12:55:32 +1100

    feat: Build flat table feature, autocomplete column generation, and update README

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Geocoded National Address File (G-NAF) Importer
 
-Imports a G-NAF extract into a new PostgreSQL database running in Docker, and creates appropriate indexes for fast querying of the data.
+Imports an Australian G-NAF extract into a new PostgreSQL database running in Docker. It will transform the data into a readable single table with every address in Australia, including a formatted address column ready for querying/filtering with appropriate indexing. You can optionally skip this process and import the raw data directly into multiple tables.
 
-The script uses as much of the data provided in a G-NAF release as possible, so it should remain compatible with future data releases. The script is written in Python 3 and requires the module Psycopg (Python-PostgreSQL Database Adapter).
+The script uses as much of the data provided in a G-NAF release as possible, so it should remain compatible with future data releases. It's is written in Python 3 and requires the module Psycopg (Python-PostgreSQL Database Adapter).
 
 The G-NAF dataset includes over 13 million Australian address records, and is provided by Australia's federal, state, and territory governments under a Creative Commons Attribution 4.0 International licence (CC BY 4.0).
-
-You can adapt this script to use an existing PostgreSQL installation if you prefer.
 
 ## Usage
 
@@ -15,15 +13,66 @@ You can adapt this script to use an existing PostgreSQL installation if you pref
 1. Extract the G-NAF folder inside the archive to the same directory as this repository (such that you have a top level directory called "G-NAF").
 1. Create a new PostgreSQL container with Docker:
     ```
-    docker run --name gnaf -d -e POSTGRESQL_PASSWORD=postgres -p 5432:5432 bitnami/postgresql:10.0
+    docker run --name gnaf -d -e POSTGRESQL_PASSWORD=postgres -p 5432:5432 postgres:latest
     ```
 1. Install Psycopg
     ```
     pip3 install psycopg2-binary
     ```
-1. Run the script to import the data:
+1. Run the importer.
+
+    Flat table import:
     ```
     python3 import_gnaf.py
     ```
 
-The import should take less than 30 minutes to complete on most hardware, and should leave you with a container of around 16GB.
+    Raw import:
+    ```
+    python3 import_gnaf.py --raw
+    ```
+
+## FAQ
+
+* **How long will it take to run? How large will the database be?**
+
+    The table below should give you a rough idea of how long it will take to build the database, and how large the container volume will be. I ran the script on my ageing laptop (i5-3320M) so you will likely get better results.
+
+    | Type | Time Taken | Final Size |
+    | --- | --- | --- |
+    | Flat table import | ~60 minutes | ~9GB |
+    | Raw import | ~30 minutes | ~16GB |
+
+* **Can I use this on macOS / Windows / Linux?**
+
+    I've only tested it on Ubuntu 18.04. I use GNU `sed` on one of the G-NAF source files to escape a backslash, this certainly won't work on macOS (includes BSD `sed`) or Windows. You can either escape the backslash manually (it's in the `NSW_ADDRESS_SITE_psv.psv` file) and comment out that line of the importer, or macOS users can install GNU `sed` with Homebrew:
+    ```
+    brew install gnu-sed --with-default-names
+    ```
+
+* **What does your flat address table/autocomplete column look like?**
+
+    You'll get a flat table of every address in Australia - here's an example for Parramatta High School:
+    
+  | address_detail_pid | street_locality_pid | locality_pid | building_name          | lot_number_prefix | lot_number | lot_number_suffix | flat_type | flat_number_prefix | flat_number | flat_number_suffix | level_type | level_number_prefix | level_number | level_number_suffix | number_first_prefix | number_first | number_first_suffix | number_last_prefix | number_last | number_last_suffix | street_name   | street_class_code | street_class_type | street_type_code | street_suffix_code | street_suffix_type | locality_name | state_abbreviation | postcode | latitude     | longitude    | geocode_type      | confidence | alias_principal | primary_secondary | legal_parcel_id | date_created | autocomplete                                                           |
+  | ------------------ | ------------------- | ------------ | ---------------------- | ----------------- | ---------- | ----------------- | --------- | ------------------ | ----------- | ------------------ | ---------- | ------------------- | ------------ | ------------------- | ------------------- | ------------ | ------------------- | ------------------ | ----------- | ------------------ | ------------- | ----------------- | ----------------- | ---------------- | ------------------ | ------------------ | ------------- | ------------------ | -------- | ------------ | ------------ | ----------------- | ---------- | --------------- | ----------------- | --------------- | ------------ | ---------------------------------------------------------------------- |
+  | GANSW711122120     | NSW325502           | NSW3184      | PARRAMATTA HIGH SCHOOL |                   |            |                   |           |                    |             |                    |            |                     |              |                     |                     | 76           | A                   |                    |             |                    | GREAT WESTERN | C                 | CONFIRMED         | HIGHWAY          |                    |                    | PARRAMATTA    | NSW                | 2150     | -33.81790658 | 150.99606946 | PROPERTY CENTROID | 1          | P               |                   | 1//DP795042     | 2010-05-02   | PARRAMATTA HIGH SCHOOL, 76A GREAT WESTERN HIGHWAY, PARRAMATTA NSW 2150 |
+  
+  So for this address, the generated autocomplete string is:
+  
+  > PARRAMATTA HIGH SCHOOL, 76A GREAT WESTERN HIGHWAY, PARRAMATTA NSW 2150
+
+* **How can I search the flat address table?**
+
+    I suggest you run a LIKE query with two wildcards on the `autocomplete` column, like follows:
+    ```
+    SELECT * FROM national_address_list WHERE autocomplete LIKE '%PARRAMATTA HIGH SCHOOL%';
+    ```
+    With the included index, this should be very quick for any query.
+    
+* **I searched for X address/business and the result was incorrect/missing!**
+
+    The data is put together by a combination of every Australian state and territory government along with the federal government. You can contact the relevant state/territory and get them to fix it in a subsequent G-NAF release.
+
+* **I got an error/the importer crashed partway through the process!**
+
+    That's frustrating! Please open an issue on this repository and I'd be happy to help you.
--- import_gnaf.py
+++ import_gnaf.py
@@ -5,11 +5,33 @@ import string
 import glob
 import os
 import re
+import argparse
 from datetime import datetime
+from psycopg2.extras import RealDictCursor
+from psycopg2.extras import execute_values
+
+parser = argparse.ArgumentParser(description='Build a G-NAF database.')
+parser.add_argument('--raw', action='store_true', help='import the raw G-NAF data without transformation')
+args = parser.parse_args()
+
+# Return the string or blank if None
+def xstr(s):
+    if s is None:
+        return ''
+    return str(s)
+
+# Return the string and a comma or blank if None
+def cstr(s):
+    if s is None:
+        return ''
+    return str(s + ", ")
 
 start_time = datetime.now()
 
-print('Starting G-NAF PostgreSQL Importer.')
+if args.raw:
+    print('Starting G-NAF PostgreSQL Importer in raw mode.')
+else:
+    print('Starting G-NAF PostgreSQL Importer in flat address table mode.')
 
 # Set up connection
 print('Connecting to PostgreSQL server...')
@@ -69,30 +91,108 @@ cursor.execute(open('G-NAF/Extras/GNAF_TableCreation_Scripts/add_fk_constraints.
 # Import address view
 print('Importing address view...')
 cursor.execute(open('G-NAF/Extras/GNAF_View_Scripts/address_view.sql', 'r').read())
+cursor.close()
+pg_connection.close()
 
-# Build indexes so our database is nice and fast
-print('Building indexes...')
-cursor.execute('CREATE INDEX address_detail_flat_type_code ON address_detail (flat_type_code)')
-cursor.execute('CREATE INDEX address_detail_level_type_code ON address_detail (level_type_code)')
-cursor.execute('CREATE INDEX address_detail_street_locality_pid ON address_detail (street_locality_pid)')
-cursor.execute('CREATE INDEX street_locality_street_locality_pid ON street_locality (street_locality_pid)')
-cursor.execute('CREATE INDEX street_locality_street_suffix_code ON street_locality (street_suffix_code)')
-cursor.execute('CREATE INDEX street_locality_street_class_code ON street_locality (street_class_code)')
-cursor.execute('CREATE INDEX locality_locality_pid ON locality (locality_pid)')
-cursor.execute('CREATE INDEX address_detail_locality_pid ON address_detail (locality_pid)')
-cursor.execute('CREATE INDEX address_default_geocode_address_detail_pid ON address_default_geocode (address_detail_pid)')
-cursor.execute('CREATE INDEX address_detail_address_detail_pid ON address_detail (address_detail_pid)')
-cursor.execute('CREATE INDEX address_default_geocode_geocode_type_code ON address_default_geocode (geocode_type_code)')
-cursor.execute('CREATE INDEX address_detail_level_geocoded_code ON address_detail (level_geocoded_code)')
-cursor.execute('CREATE INDEX locality_state_pid ON locality (state_pid)')
+if args.raw:
+    # Build indexes so our database is nice and fast
+    print('Building indexes...')
+    pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+    pg_connection.set_session(autocommit=True)
+    cursor = pg_connection.cursor()
+    cursor.execute('CREATE INDEX address_detail_flat_type_code ON address_detail (flat_type_code)')
+    cursor.execute('CREATE INDEX address_detail_level_type_code ON address_detail (level_type_code)')
+    cursor.execute('CREATE INDEX address_detail_street_locality_pid ON address_detail (street_locality_pid)')
+    cursor.execute('CREATE INDEX street_locality_street_locality_pid ON street_locality (street_locality_pid)')
+    cursor.execute('CREATE INDEX street_locality_street_suffix_code ON street_locality (street_suffix_code)')
+    cursor.execute('CREATE INDEX street_locality_street_class_code ON street_locality (street_class_code)')
+    cursor.execute('CREATE INDEX locality_locality_pid ON locality (locality_pid)')
+    cursor.execute('CREATE INDEX address_detail_locality_pid ON address_detail (locality_pid)')
+    cursor.execute('CREATE INDEX address_default_geocode_address_detail_pid ON address_default_geocode (address_detail_pid)')
+    cursor.execute('CREATE INDEX address_detail_address_detail_pid ON address_detail (address_detail_pid)')
+    cursor.execute('CREATE INDEX address_default_geocode_geocode_type_code ON address_default_geocode (geocode_type_code)')
+    cursor.execute('CREATE INDEX address_detail_level_geocoded_code ON address_detail (level_geocoded_code)')
+    cursor.execute('CREATE INDEX locality_state_pid ON locality (state_pid)')
+    cursor.close()
+    pg_connection.close()
+else:
+    print('Building flat address table from address view...')
+    pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+    pg_connection.set_session(autocommit=True)
+    cursor = pg_connection.cursor()
+    cursor.execute('CREATE TABLE national_address_list AS SELECT * FROM public.address_view')
+    cursor.execute('ALTER TABLE national_address_list ADD COLUMN autocomplete VARCHAR')
+    cursor.execute('ALTER TABLE national_address_list ADD PRIMARY KEY (address_detail_pid)')
+    cursor.close()
+    pg_connection.close()
+
+    # Build a list of every address in the country, alongside a neatly formatted autocomplete string
+    # We'll do this state by state so we don't run out of memory
+    states = ['ACT', 'NSW', 'NT', 'OT', 'QLD', 'SA', 'TAS', 'VIC', 'WA']
+    for state in states:
+        # Re-open our cursor with the ability to see column names
+        pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+        pg_connection.set_session(autocommit=True)
+        cursor = pg_connection.cursor(cursor_factory=RealDictCursor)
+        cursor.itersize = 10000
+        autocomplete_set = []
+        print('Building formatted autocomplete string for every address in %s...' %(state))
+        cursor.execute('SELECT * FROM national_address_list WHERE state_abbreviation = %s', (state,))
+        for row in cursor:
+            autocomplete = ''
+            autocomplete += cstr(row['building_name'])
+            if row['flat_type'] is not None:
+                autocomplete += xstr(row['flat_type']) + " "
+            if row['flat_number_prefix'] is not None or row['flat_number'] is not None or row['flat_number_suffix'] is not None:
+                autocomplete += xstr(row['flat_number_prefix']) + xstr(row['flat_number']) + xstr(row['flat_number_suffix']) + ", "
+            if row['number_first'] is not None or row['number_first_suffix'] is not None:
+                autocomplete += xstr(row['number_first']) + xstr(row['number_first_suffix'])
+            if row['number_last'] is not None or row['number_last_suffix'] is not None:
+                autocomplete += "-" + xstr(row['number_last']) + xstr(row['number_last_suffix']) + " "
+            else:
+                autocomplete += " "
+            autocomplete += xstr(row['street_name']) + " "
+            if row['street_type_code'] is not None:
+                autocomplete += cstr(row['street_type_code'])
+            else:
+                autocomplete += ", "
+            autocomplete += xstr(row['locality_name']) + " " + xstr(row['state_abbreviation']) + " " + xstr(row['postcode'])
+            autocomplete_set.append((row['address_detail_pid'], autocomplete))
+        cursor.close()
+        pg_connection.close()
+
+        # Bulk update every address in the database from our list
+        pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+        pg_connection.set_session(autocommit=True)
+        cursor = pg_connection.cursor()
+        print('Bulk updating every address in %s with an autocomplete string...' %(state))
+        execute_values(cursor, "UPDATE national_address_list SET autocomplete = autocomplete_set.autocomplete FROM (VALUES %s) AS autocomplete_set (address_detail_pid, autocomplete) WHERE national_address_list.address_detail_pid = autocomplete_set.address_detail_pid", autocomplete_set)
+        cursor.close()
+        pg_connection.close()
+
+    # Build a fancy GIN index
+    print('Building indexes...')
+    pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+    pg_connection.set_session(autocommit=True)
+    cursor = pg_connection.cursor()
+    cursor.execute('CREATE EXTENSION btree_gin')
+    cursor.execute('CREATE EXTENSION pg_trgm')
+    cursor.execute('CREATE INDEX national_address_list_autocomplete ON national_address_list USING GIN (autocomplete gin_trgm_ops)')
+
+    # Drop all tables besides our flat one, along with the imported view
+    cursor.execute('DROP TABLE address_alias, address_alias_type_aut, address_change_type_aut, address_default_geocode, address_detail, address_feature, address_mesh_block_2011, address_mesh_block_2016, address_site, address_site_geocode, address_type_aut, flat_type_aut, geocode_reliability_aut, geocode_type_aut, geocoded_level_type_aut, level_type_aut, locality, locality_alias, locality_alias_type_aut, locality_class_aut, locality_neighbour, locality_point, mb_2011, mb_2016, mb_match_code_aut, primary_secondary, ps_join_type_aut, state, street_class_aut, street_locality, street_locality_alias, street_locality_alias_type_aut, street_locality_point, street_suffix_aut, street_type_aut CASCADE')
+    cursor.close()
+    pg_connection.close()
 
 # Vacuum and optimise our database
 print('Vacuuming and optimising database...')
+pg_connection = psycopg2.connect('host=localhost dbname=gnaf user=postgres password=postgres')
+pg_connection.set_session(autocommit=True)
+cursor = pg_connection.cursor()
 cursor.execute('VACUUM ANALYZE')
-
-# Close our cursor and connection, we're done
 cursor.close()
 pg_connection.close()
+
 time_taken = datetime.now() - start_time
 minutes, seconds = divmod(time_taken.seconds, 60)
 print('G-NAF data was successfully imported in ' + str(minutes) + ' minutes, ' + str(seconds) + ' seconds.')
